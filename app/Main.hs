4c5872b72c90a667b8fb660b8a33f26ec751e5f6
Sunshine <yewhang2004@gmail.com>
2026-03-06 20:26:28 +0800

    Three button

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,15 @@ import Fudgets
 main :: IO()
 main = fudlogue (shellF "Button uwu" counterF)
 
-counterF = intDispF >==< mapstateF count 0 >==< (buttonF "up" >+< buttonF "Down")
+counterF = intDispF >==< mapstateF count 0 >==< buttonsF
 
-count n (Left Click) = (n+1, [n+1])
-count n (Right Click) = (n-1, [n-1])
+data Button = Up | Down | Reset deriving (Eq)
+
+buttonsF = listF [
+    (Up, buttonF "Up"),
+    (Down, buttonF "Down"),
+    (Reset, buttonF "Reset")]
+
+count n (Up, Click) = (n+1, [n+1])
+count n (Down, Click) = (n-1, [n-1])
+count n (Reset, Click) = (0, [0])
