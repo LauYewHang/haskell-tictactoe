ee4faa3f4c474d5ee261c9aebb413bc18f8d6bcf
Sunshine <yewhang2004@gmail.com>
2026-03-06 19:35:35 +0800

    Added label.

--- app/Main.hs
+++ app/Main.hs
@@ -2,4 +2,13 @@ import Fudgets
 import AllFudgets
 
 main :: IO()
-main = fudlogue (shellF "Hewwo" (labelF "Hewwo, world~" >+< quitButtonF))
+main = fudlogue (shellF "Fac uwu" facF)
+
+facF = placerF (revP verticalP) (
+    ("x! =" `labLeftOfF` intDispF) >==<
+    mapF fac >==<
+    ("x =" `labLeftOfF` intInputF))
+
+fac :: Int -> Int
+fac 0 = 1
+fac n = n * fac (n-1)
