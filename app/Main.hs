7f28b627209e265384e3e8d86688e6f6bae886e9
Sunshine <yewhang2004@gmail.com>
2026-03-06 20:20:18 +0800

    Two button uwu

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ import Fudgets
 main :: IO()
 main = fudlogue (shellF "Button uwu" counterF)
 
-counterF = intDispF >==< mapstateF count 0 >==< buttonF "up"
+counterF = intDispF >==< mapstateF count 0 >==< (buttonF "up" >+< buttonF "Down")
 
-count n Click = (n+1, [n+1])
+count n (Left Click) = (n+1, [n+1])
+count n (Right Click) = (n-1, [n-1])
