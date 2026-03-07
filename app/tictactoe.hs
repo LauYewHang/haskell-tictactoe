85b5379433c935463a197c0a1ed57f74e2c04f35
Sunshine <yewhang2004@gmail.com>
2026-03-07 11:23:30 +0800

    Added data Player to represent the player. Added type PlayerList to represent the current active players.

--- app/tictactoe.hs
+++ app/tictactoe.hs
@@ -25,5 +25,26 @@ playboard :: Playboard
 -- to generate each row, it is for j in [0..2] to generate each column, and that each column is Empty Symbol
 playboard = [[Empty | column <- [0..2]] | row <- [0..2]]
 
+-- Player
+-- used to represent Player1, Player2, or Computer
+data Player = P1 | P2 | COM deriving (Show)
+-- PlayerList type
+-- a tuple of (Player, Player), used to represent the current active player(s)
+-- e.g. (P1, P2), (P1, COM)
+type PlayerList = (Player, Player)
+
+-- calculate the winner based on the total mark
+-- given a PlayerList (x, y) to represent the active player
+-- if mark is >= 3 (three consecutive O in row / column / diagonal), then winner is x
+-- if mark is <= -3 (three consecutive -1), then winner is y
+-- otherwise, there is no winner
+calWinner :: PlayerList -> Int -> Maybe Player
+calWinner (x,y) mark
+    | mark >= 3 = Just x
+    | mark <= -3 = Just y
+    | otherwise = Nothing
+
 main = do
-    print (playboard)
+    print (calWinner (P1, COM) (-3))
+    print (calWinner (P1, COM) 0)
+    print (calWinner (P1, COM) 3)
